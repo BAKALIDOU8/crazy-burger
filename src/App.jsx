205024416ca0251228b0d65601c7ad9e784613c7
Ba Kalidou <Bakalidou8@gmail.com>
2023-09-01 11:17:39 +0200

    Maj F01

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react"
 
 function App() {
-  const [count, setCount] = useState(0)
+  //state (étatn données)
+  const [bienvenue, setPrenom] = useState("BK")
+  const [count, setCount] = useState(1)
 
+  // comportements
+  const handleClick = () => {
+    setCount(count + 1)
+  }
+
+  //affichage(render (rendu) )
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Bonjour Kalid
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+  <div>
+    <h1>Bienvenue chez nous ! </h1>
+    <p>{count}</p>
+    <button placeholder="Entre votre prénom..." >Acceder a votre espace</button>
+  </div> 
   )
 }
 
