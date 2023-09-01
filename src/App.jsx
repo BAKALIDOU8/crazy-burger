ed3a42b7c849f6d6421e34e01b09d092e819062e
Ba Kalidou <Bakalidou8@gmail.com>
2023-09-01 13:40:04 +0200

    ♻️ F01: LOGIN > Create LoginForm

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css"
-import LoginPage from "./components/pages/LoginPage"
+import LoginPage from "./components/pages/login/LoginPage"
 
 function App() {
 return <LoginPage/>
---
+++ src/components/pages/login/LoginPage.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react"
+import LoginForm from "./loginForm"
+
+export default function LoginPage() {
+  // render 
+  return ( 
+  <div>
+    <LoginForm/>
+  </div>
+    
+  )
+}
+
