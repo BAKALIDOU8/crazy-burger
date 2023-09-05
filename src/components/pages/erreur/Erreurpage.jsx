73976acb55d5c99bebce1d3ea3b6572a0fb44f1f
Ba Kalidou <Bakalidou8@gmail.com>
2023-09-05 14:46:19 +0200

    ✨ F02: LOGIN > add redirection from/to Erreurpage

--- src/components/pages/erreur/Erreurpage.jsx
+++ src/components/pages/erreur/Erreurpage.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Erreurpage() {
-  return (
-    <div>Erreurpage</div>
-  )
+  return ( 
+    <div>
+      <h1>ErreurPage</h1>
+      <Link to="/" >
+        <button>Retourner vers la page d'acceuil</button>
+      </Link>
+    </div>
+    )
 }
