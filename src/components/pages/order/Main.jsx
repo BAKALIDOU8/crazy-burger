35e497c48e8778a6715a80bec4a623f5949a53be
Ba Kalidou <Bakalidou8@gmail.com>
2023-09-12 00:52:16 +0200

    ♻️ F04: ORDER > Create Main and Navbar

---
+++ src/components/pages/order/Main.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import styled from 'styled-components';
+
+export default function Main() {
+  return (
+    <MainStyled>
+    <p>main</p>
+  </MainStyled>
+  )
+}
+
+const MainStyled = styled.div`
+      background: green;
+    flex: 1;
+`;
--- src/components/pages/order/OrderPage.jsx
+++ src/components/pages/order/OrderPage.jsx
@@ -1,6 +1,8 @@
 import React from "react"
 import { Link, useParams } from "react-router-dom"
 import styled from 'styled-components';
+import NavBar from "./NavBar";
+import Main from "./Main";
 
 export default function OrderPage() {
   // state
@@ -12,10 +14,8 @@ export default function OrderPage() {
   return ( 
   <OrderPageStyled>
     <div className="container">
-      <h1>Bonjour {username} </h1>
-        <Link to="/" >
-          <button>Déconnexion</button>
-        </Link>
+      <NavBar username={username} />
+      <Main/>
     </div>
   </OrderPageStyled>
   )
@@ -32,5 +32,8 @@ const OrderPageStyled = styled.div`
     background-color: red;
     height: 93vh;
     width: 170vh;
+    display: flex;
+    flex-direction: column;
   }
+
 `;
