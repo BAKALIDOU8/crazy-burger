73c8158b7a86a3fdc82ee57d6e68a66d715eb17e
Ba Kalidou <Bakalidou8@gmail.com>
2023-09-17 15:38:45 +0200

    💡 F05/ORDER > MAIN > Prepare code for <Basket/>

--- src/components/pages/order/Main/Main.jsx
+++ src/components/pages/order/Main/Main.jsx
@@ -6,7 +6,7 @@ import Menu from './menu';
 export default function Main() {
     return (
         <MainStyled>
-            <div className="basket">Baskets</div>
+            {/* <div className="basket">Baskets</div> */}
             <Menu/>
             
         </MainStyled>
@@ -21,11 +21,13 @@ const MainStyled = styled.div`
     border-bottom-right-radius: ${theme.borderRadius.extraRound};
     box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset ;
     display: grid;
-    grid-template-columns: 25% 1fr ;
+    grid-template-columns: 1fr ;
+    //CSS FOR BASKET
+    //grid-template-columns: 25% 1fr ;
     
-    .basket{
+    /* .basket{
         background: yellow;
-    }
+    } */
 
    
 
