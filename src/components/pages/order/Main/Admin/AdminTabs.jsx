1d5435e40dce60370cc4f3bdda590ff437044997
Ba Kalidou <Bakalidou8@gmail.com>
2023-10-21 02:27:47 +0200

    ✨  F07: ADMIN > TAB > Add label

--- src/components/pages/order/Main/Admin/AdminTabs.jsx
+++ src/components/pages/order/Main/Admin/AdminTabs.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import styled from 'styled-components';
 import Tab from '../../../../reusable-ui/Tab';
 import { FiChevronDown, FiChevronUp} from 'react-icons/fi';
+import { AiOutlinePlus } from 'react-icons/ai';
 import { theme } from '../../../../../theme';
 
 
@@ -11,8 +12,16 @@ export default function AdminTabs({isCollapsed, setIsCollapsed}) {
   }
   return (
     <AdminTabsStyled>
-      <Tab Icon={isCollapsed ? <FiChevronUp/> : <FiChevronDown/> } onClick={handleClick} 
-      className={isCollapsed ? "is-active" : ""}/>
+      <Tab 
+          label=""
+          Icon={isCollapsed ? <FiChevronUp/> : <FiChevronDown/> } 
+          onClick={handleClick} 
+          className={isCollapsed ? "is-active" : ""}/>
+      <Tab
+          label="Ajouter un produit" 
+          Icon={<AiOutlinePlus/>} 
+          onClick={handleClick} 
+          className={isCollapsed ? "is-active" : ""}/>
     </AdminTabsStyled>
   )
 }
@@ -25,6 +34,10 @@ const AdminTabsStyled = styled.div`
       background: ${theme.colors.background_dark};   
       border-color: ${theme.colors.white};
       color: ${theme.colors.white};
+    }
+
+    button{
+      margin-left: 1px;
     }
   
 `;
--- src/components/reusable-ui/Tab.jsx
+++ src/components/reusable-ui/Tab.jsx
@@ -3,10 +3,11 @@ import styled from 'styled-components';
 import { theme } from '../../theme'
 
 
-export default function Tab({Icon, onClick, className}) {
+export default function Tab({label, Icon, onClick, className}) {
   return (
     <TabStyled onClick={onClick} className={className}>
-        <div className='icon'>{Icon}</div>       
+        <div className='icon'>{Icon}</div> 
+        {label && <span className='label' >{label}</span>}       
     </TabStyled>
   )
 }
@@ -43,6 +44,10 @@ const TabStyled = styled.button`
 
     .icon{
         display: flex;
+    }
+
+    .label{
+      margin-left: 13px;
     }
 
 `;
